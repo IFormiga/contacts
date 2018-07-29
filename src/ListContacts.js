8955f3e51ea108ee98807df17682ca0685f5cf3b
igorformigaa <igorformigaa@gmail.com>
2018-07-29 19:37:27 -0300

    Adding search funcionality with component state management

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
+
 
 class ListContacts extends Component {
+    static propTypes = {
+        contacts: PropTypes.array.isRequired,
+        onDeleteContact: PropTypes.func.isRequired
+    }
+    state = {
+        query: ''
+    }
+
+    updateQuery = (query) => {
+        this.setState({ query: query.trim()  });
+    }
+
     render(){
+        let showingContacts
+        if (this.state.query) {
+            const match = new RegExp(escapeRegExp(this.state.query), 'i');
+            showingContacts = this.props.contacts.filter((contact) => match.test(contact.name))
+        }else{
+            showingContacts = this.props.contacts
+        }
+
         return(
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact) => (
-                    <li key={contact.id} className='contact-list-item'>
-                        <div className='contact-avatar' style={{
-                            backgroundImage: `url(${contact.avatarURL})`
-                        }}/>
-                        <div className='contact-details'>
-                            <p>{contact.name}</p>
-                            <p>{contact.email}</p>
-                        </div>
-                        <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
-                            Remove
-                        </button>
-                    </li>
-                ))}
-            </ol> 
+            <div className='list-contacts'>
+                <div className='list-contacts-top'>
+                    <input
+                     className='search-contacts'
+                     type='text' 
+                     placeholder='Search Contacts'
+                     value={this.state.query}
+                     onChange={(event) => this.updateQuery(event.target.value)}
+                     ></input>
+                </div>
+                <ol className='contact-list'>
+                    {showingContacts.map((contact) => (
+                        <li key={contact.id} className='contact-list-item'>
+                            <div className='contact-avatar' style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                            <div className='contact-details'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                                Remove
+                            </button>
+                        </li>
+                    ))}
+                </ol>    
+            </div>
+            
         )
     }
 }
